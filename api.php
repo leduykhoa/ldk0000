1d83e2d9befc2b6a476a6d3bc3969e3e497d9c4e
Lê Duy Khoa <ldk@LEDUYKHOA-PC.(none)>
2013-01-30 17:55:21 +0700

    kjgk

--- api.php
+++ api.php
@@ -24,8 +24,10 @@
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
+
+
 if (version_compare(phpversion(), '5.2.0', '<')) {
-    echo 'It looks like you have an invalid PHP version. Magento supports PHP 5.2.0 or newer';
+    echo 'It looks like you have an invalid PHP version. Magento supports PHP 5.2.0 or newer abc';
     exit;
 }
 error_reporting(E_ALL | E_STRICT);
